8c5173cb117cda8e38a47faf823d46f2b6090409
ernesto <ernesto>
2026-07-18 15:47:15 -0400

    start

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM php:8.2-apache
+
+# Instalar extensiones requeridas por CodeIgniter 4
+RUN apt-get update && apt-get install -y \
+    libicu-dev \
+    && docker-php-ext-configure intl \
+    && docker-php-ext-install intl mysqli pdo pdo_mysql
+
+# Habilitar mod_rewrite para las URLs amigables
+RUN a2enmod rewrite
+
+# Cambiar el DocumentRoot a la carpeta public/ de CodeIgniter
+ENV APACHE_DOCUMENT_ROOT /var/www/html/public
+RUN sed -ri -e 's!/var/www/html!${APACHE_DOCUMENT_ROOT}!g' /etc/apache2/sites-available/*.conf
+RUN sed -ri -e 's!/var/www/!${APACHE_DOCUMENT_ROOT}!g' /etc/apache2/apache2.conf /etc/apache2/conf-available/*.conf
---
+++ codeigniter/app/Database/Migrations/2026-07-18-184249_Clients.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Clients extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 5,
+                'unsiigned' => true,
+                'auto_increment' => true,
+            ],
+            'nombre' => [
+                'type' => 'VARCHAR',
+                'constraint' => 50
+            ],
+            'apellido' => [
+                'type' => 'VARCHAR',
+                'constraint' => 50
+            ]
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('invitados');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('invitados');
+    }
+}
